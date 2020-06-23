dbfcdd2ce57b8b4a682a61d0581d22039916b10d
Volpone24 <rishabgoswami2010@gmail.com>
2020-06-23 11:47:27 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Rishab Goswami',
+      zip_safe=False)
